6e16026d4e83eee7fd29317e595785ec7d8ebc9e
James Brown <jamesbbb1@gmail.com>
2020-02-18 15:20:32 -0600

    seting up folder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-340
+repository for web-340
+#contributors Professor Krasso - Bellevue University James Brown - Bellevue University
---
+++ brown-assignment-starter.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  Enter Assignment Name
+; Author: James Brown
+; Date:   Enter Date
+; Description: Enter Description
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../brown-header.js');
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display('James', 'Brown', 'Enter Exercise Name'));
+
+// Welcome message
+console.log('\n--Welcome to Exercise --');
+
