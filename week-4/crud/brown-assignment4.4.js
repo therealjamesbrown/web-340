6dbc8d35a86ade69939b1447e33359a666f04d3b
James Brown <jamesbbb1@gmail.com>
2020-03-10 12:30:45 -0500

    adding 4.4

---
+++ week-4/crud/brown-assignment4.4.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  brown-assignment.4.4.js
+; Author: Professor Krasso
+: Modifications by: James Brown
+; Date:   28 February 2019
+; Description: Demonstrates CRUD operations in a
+;              Node.js API.
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/", function(request, response) {
+  response.send("API invoked as an HTTP GET request.");
+});
+
+app.put("/", function(request, response) {
+  response.send("API invoked as an HTTP PUT request.");
+});
+
+app.post("/", function(request, response) {
+  response.send("API invoked as an HTTP POST request");
+});
+
+app.delete("/", function(request, response) {
+  response.send("API invoked as an HTTP DELETE request");
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
