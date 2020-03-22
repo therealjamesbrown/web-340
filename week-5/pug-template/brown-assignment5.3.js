b1665aa02678ce076c420cb3ea24de2c912685c9
James Brown <jamesbbb1@gmail.com>
2020-03-22 18:19:25 -0500

    adding 5.3

---
+++ week-5/pug-template/brown-assignment5.3.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title:  Exercise 5.3
+; Author: James Brown
+; Date:   3/21/2020
+; Description: Exercise 5.3
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../../brown-header.js');
+
+// Call the console.log() function and output a well-formatted header
+console.log(header.display('James', 'Brown', 'Exercise 5.3'));
+
+// Welcome message
+console.log('\n--Welcome to Exercise 5.2--');
+
+var express = require('express');
+var http = require('http');
+var pug = require('pug')
+var path = require('path');
+
+app = express();
+
+app.set('views', path.resolve(__dirname,'views'));
+
+app.set('view engine', 'pug');
+
+
+app.get('/', function(req, res){
+    res.render('index',{
+        message: "Welcome to my Pug based homepage!"
+    });
+});
+
+http.createServer(app).listen(8080, function(){
+    console.log("App started on port 8080!");
+})
+
