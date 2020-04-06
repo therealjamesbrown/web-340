ebbb88eda26644abc0dbf4f96e22e9cbbb591b81
James Brown <jamesbbb1@gmail.com>
2020-04-05 21:42:08 -0500

    uploading

---
+++ ems/models/employee.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  brown-assignment.7.4.js
+; Author: James Brown
+; Date:   4/3/2020
+; Description: 
+;===========================================
+*/
+
+//requires
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//define the employees schema
+var emsSchema = new Schema({
+    firstName: String,
+    lastName: String
+})
+
+//define the employee model
+var Employee = mongoose.model("Employee", emsSchema);
+
+//expose employee to calling files
+module.exports = Employee;
+
